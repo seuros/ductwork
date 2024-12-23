54941115fb9b2ea2695d02cf495533971b2a7f06
Tyler Ewing <tewing10@gmail.com>
2024-12-23 06:57:09 -0600

    chore: run specs in random order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,8 @@ require "ductwork"
 RSpec.configure do |config|
   config.example_status_persistence_file_path = ".rspec_status"
   config.disable_monkey_patching!
+  config.order = :random
+  Kernel.srand(config.seed)
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
