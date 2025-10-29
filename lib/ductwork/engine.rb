f5fc0f557c0c69d65fcc5ace28a04500416dce4d
Tyler Ewing <tewing10@gmail.com>
2025-10-29 07:20:06 -0500

    fix: remove configuration loading in railtie

--- lib/ductwork/engine.rb
+++ lib/ductwork/engine.rb
@@ -2,9 +2,5 @@
 
 module Ductwork
   class Engine < ::Rails::Engine
-    initializer "ductwork.load_configuration" do
-      path = Rails.root.join("config/ductwork.yml")
-      Ductwork.configuration ||= Configuration.new(path: path)
-    end
   end
 end
