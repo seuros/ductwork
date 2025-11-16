85f7e0f46f28edd8fd9ab323d82a0f186e95c344
Tyler Ewing <tewing10@gmail.com>
2025-11-16 05:53:06 -0600

    feat: validate all pipeline definitions on rails boot

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,7 @@
 
 - fix: allow steps to be chained while pipeline is expanded or divided (before collapsing or combining) - before this incorrectly raised a `CollapseError` or `CombineError`
 - feat: validate argument(s) passed to step transition DSL methods to be valid step class
+- feat: validate all pipeline definitions on rails boot
 
 ## [0.1.0]
 
--- lib/ductwork/engine.rb
+++ lib/ductwork/engine.rb
@@ -10,5 +10,15 @@ module Ductwork
       Ductwork.configuration ||= Ductwork::Configuration.new
       Ductwork.configuration.logger ||= Rails.logger
     end
+
+    initializer "ductwork.validate_definitions", after: :load_config_initializers do
+      ActiveSupport.on_load(:active_record) do
+        # Load steps and pipelines so definition validation runs and bugs
+        # can be caught simply by booting the app or running tests
+        loader = Rails.autoloaders.main
+        loader.eager_load_dir(Rails.root.join("app/steps"))
+        loader.eager_load_dir(Rails.root.join("app/pipelines"))
+      end
+    end
   end
 end
