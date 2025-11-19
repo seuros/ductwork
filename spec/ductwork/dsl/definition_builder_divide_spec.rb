5bcc7f888de7fd299ea8532bd4582f2689721811
Tyler Ewing <tewing10@gmail.com>
2025-11-19 06:12:47 -0600

    chore: break large spec file into multiple files

---
+++ spec/ductwork/dsl/definition_builder_divide_spec.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+RSpec.describe Ductwork::DSL::DefinitionBuilder do
+  let(:builder) { described_class.new }
+
+  describe "#divide" do
+    it "returns the builder instance" do
+      returned_builder = builder.start(MyFirstStep).divide(to: [MySecondStep, MyThirdStep])
+
+      expect(returned_builder).to eq(builder)
+    end
+
+    it "returns the builder instance when given a block" do
+      returned_builder = builder.start(MyFirstStep).divide(to: [MySecondStep, MyThirdStep]) do
+        puts
+      end
+
+      expect(returned_builder).to eq(builder)
+    end
+
+    it "adds new branches and steps to the definition" do
+      definition = builder
+                   .start(MyFirstStep)
+                   .divide(to: [MySecondStep, MyThirdStep])
+                   .complete
+
+      expect(definition[:nodes]).to eq(%w[MyFirstStep MySecondStep MyThirdStep])
+      expect(definition[:edges].length).to eq(3)
+      expect(definition[:edges]["MyFirstStep"]).to eq(
+        [
+          { to: %w[MySecondStep MyThirdStep], type: :divide },
+        ]
+      )
+      expect(definition[:edges]["MySecondStep"]).to eq([])
+      expect(definition[:edges]["MyThirdStep"]).to eq([])
+    end
+
+    it "yields the new branches if a block is given" do
+      expect do |block|
+        builder.start(MyFirstStep).divide(to: [MySecondStep, MyThirdStep], &block)
+      end.to yield_control
+    end
+
+    it "raises if the argument is not a class" do
+      expect do
+        builder.start(MyFirstStep).divide(to: [MySecondStep, "MyThirdStep"])
+      end.to raise_error(
+        ArgumentError,
+        "Arguments must be a valid step class"
+      )
+    end
+
+    it "raises if the argument does not have an 'execute' method" do
+      expect do
+        builder.start(MyFirstStep).divide(to: [Class.new, MyThirdStep])
+      end.to raise_error(
+        ArgumentError,
+        "Arguments must be a valid step class"
+      )
+    end
+
+    it "raises if the argument does not have `execute` with arity zero" do
+      klass = Class.new do
+        def execute(_foobar); end
+      end
+
+      expect do
+        builder.start(MyFirstStep).chain(klass)
+      end.to raise_error(
+        ArgumentError,
+        "Argument must be a valid step class"
+      )
+    end
+
+    it "raises if pipeline has not been started" do
+      expect do
+        builder.divide(to: [MyFirstStep, MySecondStep])
+      end.to raise_error(
+        described_class::StartError,
+        "Must start pipeline definition before dividing chain"
+      )
+    end
+  end
+end
---
+++ spec/ductwork/dsl/definition_builder_expand_spec.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+RSpec.describe Ductwork::DSL::DefinitionBuilder do
+  let(:builder) { described_class.new }
+
+  describe "#expand" do
+    it "returns the builder instance" do
+      returned_builder = builder.start(MyFirstStep).expand(to: MySecondStep)
+
+      expect(returned_builder).to eq(builder)
+    end
+
+    it "adds a step to the definition" do
+      definition = builder.start(MyFirstStep).expand(to: MySecondStep).complete
+
+      expect(definition[:nodes]).to eq(%w[MyFirstStep MySecondStep])
+      expect(definition[:edges].length).to eq(2)
+      expect(definition[:edges]["MyFirstStep"]).to eq(
+        [
+          { to: %w[MySecondStep], type: :expand },
+        ]
+      )
+      expect(definition[:edges]["MySecondStep"]).to eq([])
+    end
+
+    it "raises if the argument is not a class" do
+      expect do
+        builder.start(MyFirstStep).expand(to: "MySecondStep")
+      end.to raise_error(
+        ArgumentError,
+        "Argument must be a valid step class"
+      )
+    end
+
+    it "raises if the argument does not have an 'execute' method" do
+      expect do
+        builder.start(MyFirstStep).expand(to: Class.new)
+      end.to raise_error(
+        ArgumentError,
+        "Argument must be a valid step class"
+      )
+    end
+
+    it "raises if the argument does not have `execute` with arity zero" do
+      klass = Class.new do
+        def execute(_foobar); end
+      end
+
+      expect do
+        builder.start(MyFirstStep).expand(to: klass)
+      end.to raise_error(
+        ArgumentError,
+        "Argument must be a valid step class"
+      )
+    end
+
+    it "raises if pipeline has not been started" do
+      expect do
+        builder.expand(to: MyFirstStep)
+      end.to raise_error(
+        described_class::StartError,
+        "Must start pipeline definition before expanding chain"
+      )
+    end
+  end
+end
